3e3d62d4188d589a7d8380ea55e1068178c9f142
ChamsFares <78964444+ChamsFares@users.noreply.github.com>
2025-08-01 19:18:39 +0100

    Add 9_chat.py

---
+++ pages/9_chat.py
@@ -0,0 +1,11 @@
+prompt_text = """
+Analyze this handwriting or signiture.
+Provide a detailed description of the characteristics, including:
+- Overall style
+- language used
+- linguistic cues about the pychological state of the writer
+- any other relevant observations
+- any potential implications for the writer's personality or emotional state
+
+Here is the analyzed requested in french:
+"""
